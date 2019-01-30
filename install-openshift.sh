dec3dc80fa694683d1661deab17d9d4e0e6210e8
Martin <>
2019-01-30 08:15:58 +0100

    Modified hostname parameter in certificate copy

--- install-openshift.sh
+++ install-openshift.sh
@@ -137,7 +137,7 @@ systemctl enable docker
 if [ ! -f ~/.ssh/id_rsa ]; then
 	ssh-keygen -q -f ~/.ssh/id_rsa -N ""
 	cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
-	ssh -o StrictHostKeyChecking=no root@$IP "pwd" < /dev/null
+	ssh -o StrictHostKeyChecking=no root@$HOSTNAME "pwd" < /dev/null
 fi
 
 export METRICS="True"
